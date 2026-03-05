000d9762ee1daa8934643e573e847566e77b8f07
Porawat Chinpala <porawat.chi+skyaidev@skyai.co.th>
2026-03-05 10:28:15 +0700

    set up locator

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    react()
+    react({
+      babel: {
+        plugins: [
+          // other Babel plugins
+          [
+            "@locator/babel-jsx/dist",
+            {
+              env: "development",
+            },
+          ],
+        ],
+      },
+    }),
   ],
   server: {
     headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin-allow-popups',
+      "Cross-Origin-Opener-Policy": "same-origin-allow-popups",
     },
   },
-})
+});
